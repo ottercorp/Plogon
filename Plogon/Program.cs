c7d84ff01af82315eb37dc3ae9595ef73196e4de
Bluefissure <bluefissure@foxmail.com>
2023-01-01 20:56:44 -0600

    Update always build

--- Plogon/Program.cs
+++ Plogon/Program.cs
@@ -11,7 +11,7 @@ namespace Plogon;
 
 class Program
 {
-    private static readonly string[] AlwaysBuildUsers = new[] { "goaaats", "Bluefissure" };
+    private static readonly string[] AlwaysBuildUsers = new[] { "goaaats", "Bluefissure", "MapleRecall" };
     
     /// <summary>
     /// The main entry point for the application.
